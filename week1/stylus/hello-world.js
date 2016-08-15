a6699353ebdada692c0289a7c2753ea3502d9009
Chris Hinrichsen <cjhinrichsen731@yahoo.com>
2016-08-15 22:09:24 +0000

    8/15, JS Validator

---
+++ week1/stylus/hello-world.js
@@ -0,0 +1,16 @@
+// JavaScript File
+
+var nameReturn = function
+
+
+
+alert("Please enter your name : ");
+
+
+
+/* if name is null, return null
+if name is >1 .length, return scanner/printIn
+var named placed within HTML doc
+*/
+
+var nameReturn = function
---
+++ week1/stylus/js-basics.js
@@ -0,0 +1,3 @@
+// JavaScript File
+
+alert('Hello Boulder!');
